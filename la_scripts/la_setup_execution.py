fd6eefe38ad42492e8fe3dc85b004accac0b1d28
Anderson Gomes <andersonmg@gmail.com>
2024-06-27 06:48:23 -0400

    WIP: la_setup_execution

--- la_scripts/la_setup_execution.py
+++ la_scripts/la_setup_execution.py
@@ -2,22 +2,22 @@
 This script is used to automate the setup of the Python virtual environment, 
 installation of Python requirements, and the application's deploy.
 """
+
 import os
 import subprocess
 import sys
 
 
-def _run_command(command, cwd=None, shell=False):
+def _run_command(command, cwd=None):
     """
     Run a shell command in the specified directory.
 
     :param command: The command to run.
     :param cwd: The directory to run the command in.
-    :param shell: Whether to use a shell to run the command.
     """
-    result = subprocess.run(command, shell=shell, cwd=cwd)
+    result = subprocess.run(command, shell=True, cwd=cwd)
     if result.returncode != 0:
-        sys.exit(result.returncode)   
+        sys.exit(result.returncode)
 
 
 def setup_venv(current_dir: str, script_dir: str):
@@ -27,7 +27,8 @@ def setup_venv(current_dir: str, script_dir: str):
     :param current_dir: The current directory of this script.
     :param script_dir: The directory containing the `la_reset_venv.py` script.
     """
-    script_path = os.path.join(script_dir, 'la_reset_venv.py')
+    script_path = os.path.join(script_dir, "la_scripts")
+    script_path = os.path.join(script_path, "la_reset_venv.py")
     print("*** Calling la_reset_venv.py script...")
     _run_command(f"python3.11 {script_path}", cwd=current_dir)
     print("*** Virtual environment recreated!")
@@ -36,11 +37,12 @@ def setup_venv(current_dir: str, script_dir: str):
 def install_requirements(current_dir: str, script_dir: str):
     """
     Install Python requirements by running the `la_install_reqs.py` script.
-    
+
     :param current_dir: The current directory of this script.
     :param script_dir: The directory containing the `la_install_reqs.py` script.
     """
-    script_path = os.path.join(script_dir, 'la_install_reqs.py')
+    script_path = os.path.join(script_dir, "la_scripts")
+    script_path = os.path.join(script_path, "la_install_reqs.py")
     print("*** Calling la_install_reqs.py script...")
     _run_command(f"python3.11 {script_path}", cwd=current_dir)
     print("*** Python requirements installed!")
@@ -50,7 +52,8 @@ def deploy(current_dir: str, script_dir: str):
     """
     Placeholder for the deploy functionality.
     """
-    script_path = os.path.join(script_dir, 'la_deploy.py')
+    script_path = os.path.join(script_dir, "la_scripts")
+    script_path = os.path.join(script_path, "la_deploy.py")
     print("*** Calling la_deploy.py script...")
     _run_command(f"python3.11 {script_path}", cwd=current_dir)
     print("*** LA Deploy is done!")
@@ -69,14 +72,17 @@ def main():
     # Get the current script directory
     current_dir = os.path.dirname(os.path.abspath(__file__))
     # Construct path to sibling directory
-    script_dir = os.path.join(current_dir, '..', 'la-common')
-   
+    script_dir = os.path.join(current_dir, "..", "la-common")
+
     # check if script directory exists
     if not os.path.exists(script_dir):
         print(f"Script directory not found: {script_dir}")
         # call the git clone command
-        parent_dir = os.path.join(current_dir, '..')
-        _run_command(f"git clone https://github.com/LearnAnything-Organization/la-common.git", cwd=parent_dir)
+        parent_dir = os.path.join(current_dir, "..")
+        _run_command(
+            f"git clone https://github.com/LearnAnything-Organization/la-common.git",
+            cwd=parent_dir,
+        )
         print(f"Cloned the la-common repository to {script_dir}")
     # script directory exists, update it
     # call the git pull command to ensure the latest version
