ee667639b1acfdeda235888ec9ec9d5ef0622ae2
Roger Araújo <roger.rf@gmail.com>
2024-08-20 22:43:32 -0400

    Fix setup data, install requirements for tests.

--- app_scripts/app_install_reqs.py
+++ app_scripts/app_install_reqs.py
@@ -14,7 +14,7 @@ def _run_command(command, cwd=None, shell=False):
     """
     result = subprocess.run(command, shell=shell, cwd=cwd)
     if result.returncode != 0:
-        sys.exit(result.returncode)   
+        sys.exit(result.returncode)
 
 def purge_pip_cache():
     """
@@ -42,12 +42,12 @@ def upgrade_pip():
 def install_essential_git_repositories(target=None):
     """
     Installs/upgrades essential modules dependencies directly from GitHub.
-    # pip install git+https://github.com/LARS-Org/aws-common    
+    # pip install git+https://github.com/LARS-Org/aws-common
     """
     essential_git_repos = [
         "https://github.com/LARS-Org/aws-common",
     ]
-    
+
     for repo in essential_git_repos:
         print(f"*** Installing/upgrading {repo} (will be quiet)...")
         cmd_list = ["pip", "install", "--upgrade", f"git+{repo}", "--quiet"]
@@ -61,48 +61,48 @@ def install_requirements_recursively():
     Recursively traverse the project directory and install all requirements.txt files.
     """
     pip_requirements_file_list = ["requirements.txt", "requirements-dev.txt"]
-    
+
     for root, dirs, files in os.walk("."):
         # Skip certain directories
         if any(skip in root for skip in [
-            ".aws-sam", ".venv", ".git", ".pytest", "lib/python", "tests/lib/python", 
-            "experiments", "__pycache__", "node_modules", "cdk.out", "packages", 
+            ".aws-sam", ".venv", ".git", ".pytest", "lib/python", "tests/lib/python",
+            "experiments", "__pycache__", "node_modules", "cdk.out", "packages",
             "lib/python3.11/site-packages", "venv",
         ]):
             continue
-        
+
         print(f"*** Processing {root}...")
-        
+
         # exclude the __init__.py files
         files = set(files) - {"__init__.py"}
-        
+
         for pip_requirements_file in pip_requirements_file_list:
             if pip_requirements_file in files:
                 pip_requirements_path = os.path.join(root, pip_requirements_file)
                 print(f"Installing {pip_requirements_path} (will be quiet)...")
                 _run_command(["pip", "install", "-r", pip_requirements_path, "--quiet"])
 
-        # Special handling for AWS Lambda Functions
-        if files and root.startswith(("lambda", "./lambda")):
+        # Special handling for AWS Lambda Functions and tests
+        if files and root.startswith(("lambda", "./lambda", "tests", "./tests")):
             packages_dir =  os.path.join(root, "packages")
             # remove the packages directory if it exists
             if os.path.exists(packages_dir):
                 shutil.rmtree(packages_dir)
             # create the packages directory
             os.makedirs(packages_dir)
-            
+
             # install the git repositories in the packages directory
-            install_essential_git_repositories(target=packages_dir)    
-                
+            install_essential_git_repositories(target=packages_dir)
+
             if pip_requirements_file in files:
                 print(f"Installing {pip_requirements_path} (will be quiet)...")
                 # install the requirements in the packages directory
-                _run_command(["pip", "install", "-r", pip_requirements_path, 
+                _run_command(["pip", "install", "-r", pip_requirements_path,
                               "--target", packages_dir, "--upgrade", "--quiet"])
 
         # Special handling for AWS Lambda Layers
         if root.startswith(("layers", "./layers")):
-            _run_command(["pip", "install", "-r", pip_requirements_path, 
+            _run_command(["pip", "install", "-r", pip_requirements_path,
                             "--target", os.path.join(root, "lib/python3.11/site-packages"), "--upgrade", "--quiet"])
 
 def install_other_packages():
@@ -114,7 +114,7 @@ def install_other_packages():
         "black",
         "isort"
     ]
-    
+
     for package in other_packages:
         print(f"*** Installing/upgrading {package} (will be quiet)...")
         _run_command(["pip", "install", "--upgrade", package, "--quiet"])
