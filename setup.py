80adaf0fd9066bb0111b51d1086e027cf6e4743a
Roger Araújo <roger.rf@gmail.com>
2024-08-20 22:43:32 -0400

    Fix setup data, install requirements for tests.

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup, find_packages
 
 def parse_requirements(filename: str):
     """
-    Utility function to read the requirements.txt file  
+    Utility function to read the requirements.txt file
     @param filename the requirements file name
     """
     with open(filename, 'r') as file:
         return file.read().splitlines()
 
 setup(
-    name='la-common',
+    name='aws-common',
     version='0.1',
     packages=find_packages(),
     install_requires=parse_requirements('requirements.txt'),  # Include dependencies
     author='LARS AI',
-    author_email='lars@lars.com',
+    author_email='lars-ai@lars-ai.com',
     description='A shared package for common modules and constants for AWS Lambda applications',
     url='https://github.com/LARS-Org/aws-common.git',
     classifiers=[
