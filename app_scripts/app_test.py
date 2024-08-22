7880733ad8208337b8924960d8aab56fb3a683a5
Anderson Gomes <54241354+Nosredzzz@users.noreply.github.com>
2024-08-22 19:44:32 -0300

    Simplify use

---
+++ app_scripts/app_test.py
@@ -0,0 +1,12 @@
+"""
+This script is used to run all the tests for the project.
+"""
+
+def run_tests(do_log_func, run_cmd_func):
+    """
+    Run all the tests for the project.
+    """
+    do_log_func("*** Running all tests for the project...")
+    # python -m pytest tests/unit/
+    run_cmd_func(["python", "-m", "pytest", "tests/unit/"])
+    do_log_func("*** All tests passed.")
