be08aa1a7ac30ccd371e5b173373ce5d1fa7e3ee
Roger Araújo <roger.rf@gmail.com>
2024-10-22 00:05:05 -0400

    Issue 28 Rename do_log_2() to do_log() and further pack data output.

--- app_common/app_utils.py
+++ app_common/app_utils.py
@@ -4,7 +4,6 @@ General-purpose utilities.
 
 import decimal
 import json
-import numbers
 import subprocess
 import sys
 from collections import deque
@@ -75,72 +74,7 @@ def is_numeric(x) -> bool:
         return False
 
 
-def do_log(obj, title=None, log_limit: int = 150):
-    """
-    Logs an object to the console, truncating the content if large.
-    If the object is a dictionary, it logs the keys and values.
-    If the object is a list, it logs the first 2 elements, indicating it's a sample.
-    """
-
-    def _indent(indent_level: int = 1, base_chars: str = "---") -> str:
-        # Generate indentation string based on the given level
-        return "\n" + (base_chars * indent_level)
-
-    output_lines = []
-    stack = deque([(obj, 0)])
-    ellipsis_chars = "…"
-
-    while stack:
-        current_obj, level = stack.pop()
-        if isinstance(current_obj, str):
-            # Handle string objects directly, applying truncation if needed
-            output_lines.append(
-                _indent(level)
-                + (
-                    current_obj[:log_limit] + ellipsis_chars
-                    if len(current_obj) > log_limit
-                    else current_obj
-                )
-                + "\n"
-            )
-
-        elif isinstance(current_obj, dict):
-            # Handle dictionary objects, logging each key and value
-            output_lines.append(_indent(level) + f"[TYPE: {type(current_obj)}]\n")
-            for key, value in reversed(current_obj.items()):
-                stack.append((value, level + 2))
-                output_lines.append(_indent(level + 1) + key + "\n")
-
-        elif isinstance(current_obj, list):
-            # Handle list objects, logging the first 2 elements as a sample
-            output_lines.append(
-                _indent(level) + f"[TYPE: {type(current_obj)}] Sample:\n"
-            )
-            for item in reversed(current_obj[:2]):
-                stack.append((item, level + 1))
-
-        else:
-            # Default case for other object types, applying truncation if needed
-            obj_str = str(current_obj)
-            output_lines.append(
-                _indent(level)
-                + (
-                    obj_str[:log_limit] + ellipsis_chars
-                    if len(obj_str) > log_limit
-                    else obj_str
-                )
-                + "\n"
-            )
-
-    if title:
-        # Print the title if provided
-        print(title)
-
-    # Print the generated log for the given object
-    print("".join(output_lines))
-
-
-def do_log_2(obj, title=None, log_limit: int = 150, list_sample_size: int = 2):
+def do_log(obj, title=None, log_limit: int = 150, list_sample_size: int = 2):
     """
     Logs an object to the console, truncating the content if large.
     If the object is a dictionary, this method logs its keys and values.
@@ -154,30 +88,51 @@ def do_log_2(obj, title=None, log_limit: int = 150, list_sample_size: int = 2):
         """
         return base_chars * indent_level
 
-    def _is_str_or_number_or_bool(x) -> bool:
+    def _is_dict_or_list(x) -> bool:
         """
-        Returns whether the input parameter is a string, number or boolean.
+        Returns whether the input parameter is a dictionary or a list.
         """
-        return (
-            isinstance(x, str) or isinstance(x, numbers.Number) or isinstance(x, bool)
+        return isinstance(x, dict) or isinstance(x, list)
+
+    def _is_not_dict_nor_list(x) -> bool:
+        """
+        Returns whether the input parameter is not a dictionary nor a list.
+        """
+        return not _is_dict_or_list(x)
+
+    def _does_not_contain_dicts_nor_lists(iterable) -> bool:
+        """
+        Returns whether the input parameter does not contain dictionaries nor
+        lists.
+        """
+        return all([_is_not_dict_nor_list(x) for x in iterable])
+
+    def _get_dict_descriptor(d: dict) -> str:
+        """
+        Returns a descriptor of the input dictionary.
+        """
+        len_items = len(d.items())
+        return f"[TYPE: {type(d)}]; Key count = {len_items}" + (
+            "; Key/value pairs:" if len_items > 0 else ""
         )
 
-    def _only_contains_str_or_number_or_bool(iterable) -> bool:
+    def _get_list_descriptor(x: list) -> str:
         """
-        Returns whether the input parameter only contains strings, numbers or
-        booleans.
+        Returns a descriptor of the input list.
         """
-        return all([_is_str_or_number_or_bool(x) for x in iterable])
+        len_l = len(x)
+        return f"[TYPE: {type(x)}]; Size = {len_l}" + ("; Sample:" if len_l > 0 else "")
 
     output_lines = []
-    stack = deque([(obj, 0)])
+    stack = deque([(obj, 0, True)])
 
     base_chars_for_indent = "--"
     ellipsis_chars = "…"
     ellipsis_len = len(ellipsis_chars)
 
     while stack:
-        current_obj, level = stack.pop()
+        current_obj, level, print_cur_obj_type = stack.pop()
+
         if isinstance(current_obj, str):
             # Handle string objects directly, applying truncation if needed
             output_lines.append(
@@ -190,143 +145,154 @@ def do_log_2(obj, title=None, log_limit: int = 150, list_sample_size: int = 2):
             )
 
         elif isinstance(current_obj, dict):
-            # Handle dictionary objects, logging each key and value
-            output_lines.append(
-                _indent(level, base_chars_for_indent) + f"[TYPE: {type(current_obj)}]; "
-                f"Key count = {len(current_obj.items())}"
-            )
-            keys_are_simple = _only_contains_str_or_number_or_bool(current_obj.keys())
-            values_are_simple = _only_contains_str_or_number_or_bool(
-                current_obj.values()
-            )
-
-            if keys_are_simple and values_are_simple:
-                # There are only simple keys and values in the dictionary,
-                # let's try to fit as much content as we can in each line.
-                # Key/value pairs are sorted in descending order by the length
-                # of the key added to the length of the value, i.e., longer
-                # key/value pairs are processed first.
-                keys = list(current_obj.keys())
-                keys.sort(
-                    key=lambda k: len(str(k)) + len(str(current_obj[k])), reverse=True
+            # Handles dictionary objects, logging each key and value
+            if print_cur_obj_type:
+                output_lines.append(
+                    _indent(level, base_chars_for_indent)
+                    + _get_dict_descriptor(current_obj)
                 )
-                key_idx = 0
-                key_count = len(keys)
 
-                on_first_item_in_line = True
-                indented_empty_line = _indent(level + 1, base_chars_for_indent)
-                line = indented_empty_line
+            # Processes keys whose values are not lists nor dictionaries.
+            # Key/value pairs are sorted in descending order by the length of
+            # the key added to the length of the value, i.e., longer key/value
+            # pairs are processed first.
+            keys = [
+                k for k in current_obj.keys() if _is_not_dict_nor_list(current_obj[k])
+            ]
+            keys.sort(
+                key=lambda k: len(str(k)) + len(str(current_obj[k])), reverse=True
+            )
+            key_idx = 0
+            key_count = len(keys)
 
-                while key_idx < key_count:
-                    key = str(keys[key_idx])
-                    value = str(current_obj[key])
-                    key_idx += 1
+            on_first_item_in_line = True
+            indented_empty_line = _indent(level + 1, base_chars_for_indent)
+            line = indented_empty_line
 
-                    # Stores pending content
-                    if len(line) >= log_limit:
-                        output_lines.append(line)
-                        on_first_item_in_line = True
-                        line = indented_empty_line
+            while key_idx < key_count:
+                key = str(keys[key_idx])
+                value = str(current_obj[key])
+                key_idx += 1
 
-                    # First try: full key, full value
-                    cur_item_preamble = "" if on_first_item_in_line else " "
-                    on_first_item_in_line = False
-                    cur_key_and_val = cur_item_preamble + key + "=" + value
+                # Stores pending content
+                if len(line) >= log_limit:
+                    output_lines.append(line)
+                    on_first_item_in_line = True
+                    line = indented_empty_line
+
+                # First try: full key, full value
+                cur_item_preamble = "" if on_first_item_in_line else " "
+                on_first_item_in_line = False
+                cur_key_and_val = cur_item_preamble + key + "=" + value
+
+                if len(line) + len(cur_key_and_val) <= log_limit:
+                    line += cur_key_and_val
+                    continue
+
+                # Second try: full key, partial value
+                cur_key_and_val = cur_item_preamble + key + "="
+                chars_left = log_limit - len(line) - len(cur_key_and_val) - ellipsis_len
+
+                if chars_left > 0:
+                    partial_value = value[0 : max(0, chars_left)] + ellipsis_chars
+                    cur_key_and_val += partial_value
 
                     if len(line) + len(cur_key_and_val) <= log_limit:
                         line += cur_key_and_val
                         continue
 
-                    # Second try: full key, partial value
-                    cur_key_and_val = cur_item_preamble + key + "="
-                    chars_left = (
-                        log_limit - len(line) - len(cur_key_and_val) - ellipsis_len
+                # Third try: partial key, full value
+                chars_left = (
+                    log_limit
+                    - len(line)
+                    - len(cur_item_preamble)
+                    - ellipsis_len
+                    - len("=")
+                    - len(value)
+                )
+
+                if chars_left > 0:
+                    partial_key = key[0 : max(0, chars_left)] + ellipsis_chars
+                    cur_key_and_val = cur_item_preamble + partial_key + "=" + value
+
+                    if len(line) + len(cur_key_and_val) <= log_limit:
+                        line += cur_key_and_val
+                        continue
+
+                # Fourth try: partial key, partial value
+                chars_left = (
+                    log_limit
+                    - len(line)
+                    - len(cur_item_preamble)
+                    - ellipsis_len
+                    - len("=")
+                    - ellipsis_len
+                )
+
+                if chars_left > 1:
+                    partial_len = chars_left // 2
+                    partial_key = (
+                        key
+                        if len(key) <= partial_len
+                        else key[0 : max(0, partial_len)] + ellipsis_chars
+                    )
+                    partial_value = (
+                        value
+                        if len(value) <= partial_len
+                        else value[0 : max(0, partial_len)] + ellipsis_chars
+                    )
+                    cur_key_and_val = (
+                        cur_item_preamble + partial_key + "=" + partial_value
                     )
 
-                    if chars_left > 0:
-                        partial_value = value[0 : max(0, chars_left)] + ellipsis_chars
-                        cur_key_and_val += partial_value
+                    if len(line) + len(cur_key_and_val) <= log_limit:
+                        line += cur_key_and_val
+                        continue
 
-                        if len(line) + len(cur_key_and_val) <= log_limit:
-                            line += cur_key_and_val
-                            continue
-
-                    # Third try: partial key, full value
-                    chars_left = (
-                        log_limit
-                        - len(line)
-                        - len(cur_item_preamble)
-                        - ellipsis_len
-                        - len("=")
-                        - len(value)
-                    )
-
-                    if chars_left > 0:
-                        partial_key = key[0 : max(0, chars_left)] + ellipsis_chars
-                        cur_key_and_val = cur_item_preamble + partial_key + "=" + value
-
-                        if len(line) + len(cur_key_and_val) <= log_limit:
-                            line += cur_key_and_val
-                            continue
-
-                    # Fourth try: partial key, partial value
-                    chars_left = (
-                        log_limit
-                        - len(line)
-                        - len(cur_item_preamble)
-                        - ellipsis_len
-                        - len("=")
-                        - ellipsis_len
-                    )
-
-                    if chars_left > 1:
-                        partial_len = chars_left // 2
-                        partial_key = (
-                            key
-                            if len(key) <= partial_len
-                            else key[0 : max(0, partial_len)] + ellipsis_chars
-                        )
-                        partial_value = (
-                            value
-                            if len(value) <= partial_len
-                            else value[0 : max(0, partial_len)] + ellipsis_chars
-                        )
-                        cur_key_and_val = (
-                            cur_item_preamble + partial_key + "=" + partial_value
-                        )
-
-                        if len(line) + len(cur_key_and_val) <= log_limit:
-                            line += cur_key_and_val
-                            continue
-
-                    # We could not fit the key/value pair into the current line.
-                    # Let's start a new line and try to process the key/value
-                    # pair again.
-                    if line != indented_empty_line:
-                        output_lines.append(line)
-                        on_first_item_in_line = True
-                        line = indented_empty_line
-                        key_idx -= 1
-
-                # Stores pending content
+                # We could not fit the key/value pair into the current line.
+                # Let's start a new line and try to process the key/value
+                # pair again.
                 if line != indented_empty_line:
                     output_lines.append(line)
-            else:
-                # There are complex elements in the list, let's stack them for
-                # later processing
-                for key, value in reversed(current_obj.items()):
-                    stack.append((value, level + 2))
-                    output_lines.append(_indent(level + 1, base_chars_for_indent) + key)
+                    on_first_item_in_line = True
+                    line = indented_empty_line
+                    key_idx -= 1
+
+            # Stores pending content
+            if line != indented_empty_line:
+                output_lines.append(line)
+
+            # Processes keys whose values are dictionaries
+            keys = [k for k in current_obj.keys() if isinstance(current_obj[k], dict)]
+            keys.sort()
+
+            for key in keys:
+                next_dict = current_obj[key]
+                stack.append((next_dict, level + 1, False))
+                stack.append(
+                    (str(key) + "=" + _get_dict_descriptor(next_dict), level + 1, False)
+                )
+
+            # Processes keys whose values are lists
+            keys = [k for k in current_obj.keys() if isinstance(current_obj[k], list)]
+            keys.sort()
+
+            for key in keys:
+                next_list = current_obj[key]
+                stack.append((next_list, level + 1, False))
+                stack.append(
+                    (str(key) + "=" + _get_list_descriptor(next_list), level + 1, False)
+                )
 
         elif isinstance(current_obj, list):
-            # Handle list objects, logging the first 2 elements as a sample
-            output_lines.append(
-                _indent(level, base_chars_for_indent) + f"[TYPE: {type(current_obj)}]; "
-                f"Size = {len(current_obj)}; Sample:"
-            )
-            elems_are_simple = _only_contains_str_or_number_or_bool(current_obj)
+            # Handles list objects, logging the first few elements as a sample
+            if print_cur_obj_type:
+                output_lines.append(
+                    _indent(level, base_chars_for_indent)
+                    + _get_list_descriptor(current_obj)
+                )
 
-            if elems_are_simple:
+            if _does_not_contain_dicts_nor_lists(current_obj):
                 # There are only simple elements in the list, let's try to fit
                 # as much content as we can in each line
                 elem_idx = 0
@@ -381,8 +347,10 @@ def do_log_2(obj, title=None, log_limit: int = 150, list_sample_size: int = 2):
                 if line != indented_empty_line:
                     output_lines.append(line)
             else:
-                for item in reversed(current_obj[:2]):
-                    stack.append((item, level + 1))
+                # There are complex elements in the list, let's stack them for
+                # later processing
+                for item in reversed(current_obj[:list_sample_size]):
+                    stack.append((item, level + 1, _is_dict_or_list(item)))
 
         else:
             # Default case for other object types, applying truncation if needed
