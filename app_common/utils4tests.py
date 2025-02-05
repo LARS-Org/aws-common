9f3f2110bf5e9aa20929905805832a81632efb46
Roger Araújo <roger.rf@gmail.com>
2025-02-04 21:34:20 -0500

    Issue 124 Add functions get_fake_aws_lambda_context() and get_basic_aws_lambda_event() to utils4tests.py.

--- app_common/utils4tests.py
+++ app_common/utils4tests.py
@@ -4,6 +4,10 @@ This module contains utility functions meant to be used in tests.
 
 import os
 
+# Constants for export
+CONTENT_TYPE = "Content-Type"
+APPLICATION_JSON = "application/json"
+
 
 def set_aws_environment_variables(
     aws_access_key_id: str,
@@ -32,3 +36,32 @@ def set_fake_aws_environment_variables(aws_default_region: str = "us-east-1"):
     set_aws_environment_variables(
         "testing", "testing", "testing", "testing", aws_default_region
     )
+
+
+def get_fake_aws_lambda_context() -> dict:
+    """
+    Returns a dictionary containing fake values for information typically found in AWS
+    Lambda Context objects. This is meant to be used mainly for testing purposes.
+    """
+    return {
+        "aws_request_id": "testing",
+        "log_group_name": "testing",
+        "log_stream_name": "testing",
+        "function_name": "testing",
+        "function_version": "1",
+        "invoked_function_arn": "testing",
+        "memory_limit_in_mb": "128",
+    }
+
+
+def get_basic_aws_lambda_event(body: str | None = "") -> dict:
+    """
+    Returns a dictionary containing basic values for an AWS Lambda event.
+    The keys present in the result dictionary are "body", whose value can be
+    provided by the caller, and "headers", which is also a dictionary containing
+    only the key "Content-Type" bound to value "application/json".
+    """
+    return {
+        "body": body,
+        "headers": {CONTENT_TYPE: APPLICATION_JSON},
+    }
